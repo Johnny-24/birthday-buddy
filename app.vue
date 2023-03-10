51ca7b421fd33a58b035cd1f539ee7ec8a2bd9d7
Johnny <dos_24@inbox.ru>
2023-03-10 17:17:33 +0500

    add test prisma

--- app.vue
+++ app.vue
@@ -1,5 +1,21 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <Container>
+      <Test></Test>
+    </Container>
+
+
+
+
+    <!-- <ul>
+      <li v-for="item in data" :key="item.id">
+        <div>{{ item.id }}</div>
+        <div>{{ item.content }}</div>
+      </li>
+    </ul> -->
   </div>
 </template>
+
+<script setup>
+
+</script>
